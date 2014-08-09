0ce54b2f3457940f3974566c9e8f1e8097d95955
Diego Rodriguez <drodriguez@opengov.com>
2014-08-08 17:34:26 -0700

    syntax error

--- s3deploy.sh
+++ s3deploy.sh
@@ -111,7 +111,7 @@ s3d_sync() {
     if [ -z $acl ]; then acl='private'; fi
 
     set -x
-    if [ "$TRAVIS_PULL_REQUEST" = "false" ]
+    if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
 	aws s3 sync "${@:4:num_extra}" --acl "$acl" "$local_dir" "s3://$s3_path"
     fi
     set +x
